03910a62411e6843532b2d0da065c4a27b82e5f3
user42 <psaumet@student.42.fr>
2022-02-01 14:28:29 +0100

    Adding srcs/ft_init.c

---
+++ srcs/ft_init.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_init.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaumet <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/06 10:47:02 by psaumet           #+#    #+#             */
+/*   Updated: 2020/01/30 14:52:29 by psaumet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+/*
+**	This file contains the functions to initialize the structures.
+*/
+
+void	ft_init_t_data(t_data *val)
+{
+	val->flags = 0;
+	val->pt_flags = &val->flags;
+	val->width = 0;
+	val->pt_width = &val->width;
+	val->prec = 0;
+	val->pt_prec = &val->prec;
+	val->qual = 0;
+	val->pt_qual = &val->qual;
+	val->count = 0;
+	val->pt_count = &val->count;
+	val->prec2 = 0;
+	val->pt_prec2 = &val->prec2;
+}
+
+void	ft_init_t(t_data *val)
+{
+	val->flags = 0;
+	val->pt_flags = &val->flags;
+	val->width = 0;
+	val->pt_width = &val->width;
+	val->prec = 0;
+	val->pt_prec = &val->prec;
+	val->qual = 0;
+	val->pt_qual = &val->qual;
+	val->prec2 = 0;
+	val->pt_prec2 = &val->prec2;
+}
+
+void	ft_init_t_tmp(t_tmp *val)
+{
+	val->pt_c = &val->c;
+	val->sign = 0;
+	val->pt_sign = &val->sign;
+	val->i = 0;
+	val->pt_i = &val->i;
+	val->res = 0;
+	val->pt_res = &val->res;
+}
