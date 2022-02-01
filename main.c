b086aa88dec9ab08b04255dd9befb175ac289d9b
user42 <psaumet@student.42.fr>
2022-02-01 14:44:55 +0100

    testing with %d, works

--- main.c
+++ main.c
@@ -8,6 +8,9 @@ int		main( void )
 	str = "Hello World!";
 	ft_printf("The string 'str' contains: %s\n", str);
 	printf("The string 'str' contains: %s\n", str);
+
+	ft_printf("The number is: %d\n", 1052 + 955212174 - 485745);
+	printf("The number is: %d\n", 1052 + 955212174 - 485745);
 	return (0);
 }
 //gcc main.c -L. -lftprintf && ./a.out
